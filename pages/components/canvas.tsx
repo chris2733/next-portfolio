942b144c94cabc7dc1d75f1beaa4378ee324ac0d
Chris Law <chris_law9208@hotmail.co.uk>
2022-10-10 01:47:22 +0100

    building railings

--- pages/components/canvas.tsx
+++ pages/components/canvas.tsx
@@ -292,6 +292,35 @@ function drawBuilding(
 	);
 
 	// randomly draw railing on top
+	if (randomBuildingId > 0.4) {
+		let railingsWidth: number = width - radius * 2;
+		let railingsStartX: number = startX + (width - railingsWidth) / 2;
+		let railingHeight: number = 6;
+		let railingsStartY: number = startY - railingHeight;
+		let railingThickness: number = 2;
+		let railingGap: number = 9;
+		paintbrush.beginPath();
+		paintbrush.fillStyle = fill;
+		paintbrush.fillRect(
+			railingsStartX,
+			railingsStartY,
+			railingsWidth,
+			railingThickness
+		);
+		// railings with gap and width, get the remainder of this from width, so they all fit perfectly - flexible gap ofc
+		const railings: number = railingGap + railingThickness;
+		const railingsNum: number = railingsWidth / railings;
+		// calc the gap at the end, usually some space left, add it to the gap + railing thickness for an accurate spacing
+		const remainingGap: number = railingsWidth % railings;
+		const space: number =
+			railingGap + railingThickness + remainingGap / railingsNum;
+		// const space = railingsWidth / railingsNum;
+		for (let i = 0; i <= railingsWidth; i += space) {
+			paintbrush.beginPath();
+			paintbrush.fillStyle = fill;
+			paintbrush.fillRect(railingsStartX + i, railingsStartY, 2, railingHeight);
+		}
+	}
 
 	// maybe if skyscraper add tower top with antenna?
 }
