674585f7305d7f84fad6edeb9208504babf07b5a
Chris Law <chris_law9208@hotmail.co.uk>
2022-10-11 01:17:04 +0100

    Draw crane, antenna, split railing up

--- pages/components/canvas.tsx
+++ pages/components/canvas.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from "react";
+import { start } from "repl";
 
 // **********
 // todo
 // add lamposts
 // cars randomly coming accross
+// make canvas render with less frames per second
+// maybe make sun/moon/sky/change with time, on a really slow cycle
 
 export default function Canvas({ data }: { data: any }) {
 	// console.log(data);
@@ -295,35 +298,17 @@ function drawBuilding(
 		windowColour
 	);
 
+	// add antenna randomly to buildings thinner than 200
+	if (width < 100 && randomBuildingId > 0.7) {
+		drawAntenna(paintbrush, startX, startY, fill, width, randomBuildingId2);
+	}
 	// randomly draw railing on top
-	if (randomBuildingId > 0.4) {
-		let railingsWidth: number = width - radius * 2;
-		let railingsStartX: number = startX + (width - railingsWidth) / 2;
-		let railingHeight: number = 6;
-		let railingsStartY: number = startY - railingHeight;
-		let railingThickness: number = 2;
-		let railingGap: number = 9;
-		paintbrush.beginPath();
-		paintbrush.fillStyle = fill;
-		paintbrush.fillRect(
-			railingsStartX,
-			railingsStartY,
-			railingsWidth,
-			railingThickness
-		);
-		// railings with gap and width, get the remainder of this from width, so they all fit perfectly - flexible gap ofc
-		const railings: number = railingGap + railingThickness;
-		const railingsNum: number = railingsWidth / railings;
-		// calc the gap at the end, usually some space left, add it to the gap + railing thickness for an accurate spacing
-		const remainingGap: number = railingsWidth % railings;
-		const space: number =
-			railingGap + railingThickness + remainingGap / railingsNum;
-		// const space = railingsWidth / railingsNum;
-		for (let i = 0; i <= railingsWidth; i += space) {
-			paintbrush.beginPath();
-			paintbrush.fillStyle = fill;
-			paintbrush.fillRect(railingsStartX + i, railingsStartY, 2, railingHeight);
-		}
+	else if (randomBuildingId > 0.2 && randomBuildingId < 0.7) {
+		drawRailing(paintbrush, startX, startY, fill, width, radius);
+	}
+	// randomly draw crane on top
+	else if (randomBuildingId < 0.15) {
+		drawCrane(paintbrush, startX, startY, fill, width, randomBuildingId2);
 	}
 
 	// maybe if skyscraper add tower top with antenna?
@@ -408,4 +393,136 @@ function drawBuildingWindows(
 			paintbrush.fillRect(x, y, windowWidth, windowHeight);
 		}
 	}
+}
+
+function drawRailing(
+	paintbrush: any,
+	startX: number,
+	startY: number,
+	fill: string,
+	width: number,
+	radius: number
+) {
+	let railingsWidth: number = width - radius * 2;
+	let railingsStartX: number = startX + (width - railingsWidth) / 2;
+	let railingHeight: number = 6;
+	let railingsStartY: number = startY - railingHeight;
+	let railingThickness: number = 2;
+	let railingGap: number = 9;
+	paintbrush.beginPath();
+	paintbrush.fillStyle = fill;
+	paintbrush.fillRect(
+		railingsStartX,
+		railingsStartY,
+		railingsWidth,
+		railingThickness
+	);
+	// railings with gap and width, get the remainder of this from width, so they all fit perfectly - flexible gap ofc
+	const railings: number = railingGap + railingThickness;
+	const railingsNum: number = railingsWidth / railings;
+	// calc the gap at the end, usually some space left, add it to the gap + railing thickness for an accurate spacing
+	const remainingGap: number = railingsWidth % railings;
+	const space: number =
+		railingGap + railingThickness + remainingGap / railingsNum;
+	// const space = railingsWidth / railingsNum;
+	for (let i = 0; i <= railingsWidth; i += space) {
+		paintbrush.beginPath();
+		paintbrush.fillStyle = fill;
+		paintbrush.fillRect(railingsStartX + i, railingsStartY, 2, railingHeight);
+	}
+}
+
+function drawAntenna(
+	paintbrush: any,
+	startX: number,
+	startY: number,
+	fill: string,
+	width: number,
+	randomBuildingId2: number
+) {
+	// add to the centerish
+	const x: number = startX + (width * 0.4 + randomBuildingId2 * (width * 0.4));
+	// random height around 50ish
+	const height: number = 20 + randomBuildingId2 * 30;
+	const antennaWidth: number = 3;
+	paintbrush.beginPath();
+	paintbrush.fillStyle = fill;
+	paintbrush.fillRect(x, startY, antennaWidth, -height);
+	// 2/3 chance to have a little one next door
+	if (randomBuildingId2 > 0.3) {
+		// place smaller antenna to side furthest from edge
+		const antennaSpace: number = 10;
+		const smallAntennaX: number =
+			x > width / 2 ? x - antennaSpace : x + antennaSpace;
+		// random height around 20ish
+		const smallAntennaHeight: number = 10 + randomBuildingId2 * 16;
+		paintbrush.beginPath();
+		paintbrush.fillStyle = fill;
+		paintbrush.fillRect(
+			smallAntennaX,
+			startY,
+			antennaWidth,
+			-smallAntennaHeight
+		);
+	}
+}
+
+function drawCrane(
+	paintbrush: any,
+	startX: number,
+	startY: number,
+	fill: string,
+	width: number,
+	randomBuildingId2: number
+) {
+	// start near the centreish
+	const x: number = startX + (width * 0.4 + randomBuildingId2 * (width * 0.4));
+	// randomly choose the direction of crane
+	const direction: string = randomBuildingId2 > 0.5 ? "left" : "right";
+	const legHeight: number = 5;
+	const legWidth: number = 2;
+	const bodyHeight: number = 6;
+	const bodyWidth: number = 16;
+	const neckLength: number = 6 + Math.floor(randomBuildingId2 * 5); // random neck length, to be looped over
+	const neckWidth: number = 2;
+	const neckHeight: number = 3; //height of each neck segment
+	paintbrush.beginPath();
+	paintbrush.fillStyle = fill;
+	paintbrush.fillRect(x, startY, legWidth, -legHeight); // legs
+	paintbrush.fillRect(
+		x - bodyWidth / 2,
+		startY - legHeight,
+		bodyWidth,
+		-bodyHeight
+	); // body
+	// draw the neck
+	const neckStart = startY - legHeight - bodyHeight + 1;
+	for (let i = 0; i < neckLength; i++) {
+		paintbrush.beginPath();
+		paintbrush.fillStyle = fill;
+		// move each neck segment up by 2*i, to the side by a neckwidth
+		// direction picks if its added or taken away
+		const directionX: number =
+			direction === "left" ? x - i * neckWidth : x + i * neckWidth;
+		paintbrush.fillRect(directionX, neckStart - i * 2, 2, -neckHeight);
+	}
+	// add the dangly bit
+	const craneDangleLength: number = neckLength * 2 + 8 * randomBuildingId2;
+	const craneDangleX: number =
+		direction === "left"
+			? x - neckLength * neckWidth
+			: x + neckLength * neckWidth;
+	paintbrush.fillRect(
+		craneDangleX,
+		neckStart - neckLength * 2,
+		2,
+		craneDangleLength
+	);
+	// little end bit
+	paintbrush.fillRect(
+		craneDangleX - 2,
+		neckStart - neckLength * 2 + craneDangleLength,
+		6,
+		2
+	);
 }
