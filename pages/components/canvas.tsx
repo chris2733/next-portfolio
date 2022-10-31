4373481d74b06a45c6aa3f4deb62b1ce57c08d1a
Chris Law <chris_law9208@hotmail.co.uk>
2022-10-31 23:58:37 +0000

    Draw stars!

--- pages/components/canvas.tsx
+++ pages/components/canvas.tsx
@@ -228,7 +228,13 @@ export default function Canvas({ data }: { data: any }) {
 
 	const drawOnce = (paintbrush: any) => {
 		// draw sky
-		drawSky(paintbrush, width, height, currentSkyLightGradients);
+		drawSky(
+			paintbrush,
+			width,
+			height,
+			currentSkyLight,
+			currentSkyLightGradients
+		);
 		// sun/moon positioning
 		drawSunMoon(
 			paintbrush,
@@ -340,8 +346,10 @@ function drawSky(
 	paintbrush: any,
 	width: number,
 	height: number,
+	currentSkyLight: string,
 	currentSkyLightGradients?: { stop: number; color: string }[]
 ) {
+	paintbrush.beginPath();
 	paintbrush.rect(0, 0, width, height);
 	// add linear gradient
 	var grd = paintbrush.createLinearGradient(width / 2, height, width / 2, 0);
@@ -352,6 +360,22 @@ function drawSky(
 	});
 	paintbrush.fillStyle = grd;
 	paintbrush.fill();
+	// draw stars if sky dark
+	let startX: number = -20;
+	let starSpaceMin: number = 7;
+	let starSpaceMax: number = 15;
+	let starColour: string = "#ffffff99";
+	if (currentSkyLight === "nauticalDusk" || "night" || "nadir" || "nightEnd") {
+		for (
+			let xPos = startX;
+			xPos < width;
+			xPos += randomIntFromInterval(starSpaceMin, starSpaceMax)
+		) {
+			let randomY = height * Math.random();
+			paintbrush.fillStyle = starColour;
+			paintbrush.fillRect(xPos, randomY, 1, 1);
+		}
+	}
 }
 
 function drawSunMoon(
