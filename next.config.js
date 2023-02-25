01e1e6ebedcb8aa9e53f6b16514a47eecf2411e0
Chris Law <chris_law9208@hotmail.co.uk>
2023-02-25 20:58:12 +0800

    fix: strictmode false with note

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // false for dev, true for prod
   swcMinify: true,
   i18n: {
     locales: ["en-GB", "en-US"],
