eeb4d3d89ba30020cb1e57c53cfd7e728f61441c
Chris Law <chris_law9208@hotmail.co.uk>
2023-02-27 01:10:56 +0800

    feat: add blendhexcolors func

--- pages/components/canvasSky.tsx
+++ pages/components/canvasSky.tsx
@@ -135,27 +135,3 @@ export default function Canvas({
 
   return <canvas ref={canvasEl} height={height} width={width}></canvas>;
 }
-
-// from here
-// lighten colour, colour to and from, c0 and c1 respectively
-// function blendHexColors(c0: string, c1: string, p: number) {
-// 	var f = parseInt(c0.slice(1), 16),
-// 		t = parseInt(c1.slice(1), 16),
-// 		R1 = f >> 16,
-// 		G1 = (f >> 8) & 0x00ff,
-// 		B1 = f & 0x0000ff,
-// 		R2 = t >> 16,
-// 		G2 = (t >> 8) & 0x00ff,
-// 		B2 = t & 0x0000ff;
-// 	return (
-// 		"#" +
-// 		(
-// 			0x1000000 +
-// 			(Math.round((R2 - R1) * p) + R1) * 0x10000 +
-// 			(Math.round((G2 - G1) * p) + G1) * 0x100 +
-// 			(Math.round((B2 - B1) * p) + B1)
-// 		)
-// 			.toString(16)
-// 			.slice(1)
-// 	);
-// }
