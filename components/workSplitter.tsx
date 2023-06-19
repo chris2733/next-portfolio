aec07d26718d23178f6dd1a7fe1517cd06e1b4c4
Chris Law <chris_law9208@hotmail.co.uk>
2023-06-19 22:23:17 +0100

    fix: better worksplitter formatting

--- components/workSplitter.tsx
+++ components/workSplitter.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function WordSplitter({
   text,
@@ -7,7 +8,7 @@ export default function WordSplitter({
   initialDelay,
   wordDelay,
   wordClass,
-  speed = "duration-500",
+  speed = "duration-300",
 }: {
   text?: string;
   boldText?: string;
@@ -17,6 +18,7 @@ export default function WordSplitter({
   wordClass?: string;
   speed?: string;
 }) {
+  const [boldTextLoaded, setboldTextLoaded] = useState<boolean>(false);
   // check over this, needed to check if text was undefined so it didnt throw a non iterable error
   // and theres some repetition
   const splittext: string[] =
@@ -29,90 +31,102 @@ export default function WordSplitter({
   const boldTextModifier =
     boldText !== undefined ? boldText.length * (wordDelay ? wordDelay : 0) : 0;
 
-  return (
-    <div className={`px-1 group hover:px-0 ${speed}`}>
-      {splitboldtext !== undefined &&
-        boldText !== undefined &&
-        splitboldtext.map((el, i, arr) => (
-          <span key={`boldwordsplitter${i}`}>
-            <span className="overflow-hidden inline-block">
-              <motion.span
-                initial="hidden"
-                animate="visible"
-                variants={{
-                  hidden: {
-                    y: "100%",
-                  },
-                  visible: {
-                    y: "0",
-                    transition: {
-                      delay:
-                        i * (wordDelay ? wordDelay : 0.2) +
-                        (initialDelay ? initialDelay + boldTextModifier : 0),
-                    },
-                  },
-                }}
-                className={`${speed} inline-block mr-[0.2em] font-medium ${wordClass} ${boldTextClass}`}
-              >
-                {`${el} `}
-              </motion.span>
-            </span>
-            {i == arr.length - 1 && (
-              <span className="overflow-hidden inline-block">
-                <motion.span
-                  initial="hidden"
-                  animate="visible"
-                  variants={{
-                    hidden: {
-                      y: "100%",
-                    },
-                    visible: {
-                      y: "0",
-                      transition: {
-                        delay:
-                          i * (wordDelay ? wordDelay : 0.2) +
-                          (initialDelay ? initialDelay + boldTextModifier : 0),
-                      },
-                    },
-                  }}
-                  className={`${speed} inline-block mr-[0.2em] font-medium px-0 group-hover:px-1 ${wordClass} ${boldTextClass}`}
-                >
-                  {` - `}
-                </motion.span>
-              </span>
-            )}
-          </span>
-        ))}
-      {splittext !== undefined &&
-        splittext.map((el, i, arr) => (
-          <span
-            className="overflow-hidden inline-block"
-            key={`wordsplitter${i}`}
+  // TODO: add types here
+  const boldTextArray =
+    splitboldtext &&
+    splitboldtext.map((el, i, arr) => (
+      <span key={`boldwordsplitter${i}`}>
+        <span className="overflow-hidden inline-block">
+          <motion.span
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                y: "100%",
+              },
+              visible: {
+                y: "0",
+                transition: {
+                  delay:
+                    i * (wordDelay ? wordDelay : 0.2) +
+                    (initialDelay ? initialDelay + boldTextModifier : 0),
+                },
+              },
+            }}
+            className={`${speed} inline-block mr-[0.2em] font-medium ${wordClass} ${boldTextClass}`}
+            onAnimationComplete={() => {
+              if (i + 1 === arr.length) {
+                setboldTextLoaded(true);
+              }
+            }}
           >
-            <motion.span
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  y: "100%",
+            {`${el} `}
+          </motion.span>
+        </span>
+      </span>
+    ));
+
+  const normalTextArray =
+    splittext &&
+    splittext.map((el, i, arr) => (
+      <span className="overflow-hidden inline-block" key={`wordsplitter${i}`}>
+        <motion.span
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              y: "100%",
+            },
+            visible: {
+              y: "0",
+              transition: {
+                delay:
+                  i * (wordDelay ? wordDelay : 0.2) +
+                  (initialDelay ? initialDelay + boldTextModifier : 0),
+              },
+            },
+          }}
+          className={`${speed} inline-block ${
+            i !== arr.length - 1 && "mr-[0.2em]"
+          } ${wordClass && wordClass}`}
+        >
+          {`${el} `}
+        </motion.span>
+      </span>
+    ));
+
+  return (
+    <div className={`px-1 group hover:px-0 highlightedlink-parent ${speed}`}>
+      {splitboldtext !== undefined && boldText !== undefined && (
+        <span className={`highlightedlink ${boldTextLoaded && "__visible"}`}>
+          {boldTextArray}
+        </span>
+      )}
+      {splitboldtext !== undefined && splittext !== undefined && (
+        <span className="overflow-hidden inline-block">
+          <motion.span
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                y: "100%",
+              },
+              visible: {
+                y: "0",
+                transition: {
+                  delay:
+                    splitboldtext.length * (wordDelay ? wordDelay : 0.2) +
+                    (initialDelay ? initialDelay + boldTextModifier : 0),
                 },
-                visible: {
-                  y: "0",
-                  transition: {
-                    delay:
-                      i * (wordDelay ? wordDelay : 0.2) +
-                      (initialDelay ? initialDelay + boldTextModifier : 0),
-                  },
-                },
-              }}
-              className={`${speed} inline-block ${
-                i !== arr.length - 1 && "mr-[0.2em]"
-              } ${wordClass && wordClass}`}
-            >
-              {`${el} `}
-            </motion.span>
-          </span>
-        ))}
+              },
+            }}
+            className={`${speed} inline-block mr-[0.2em] font-medium px-0 group-hover:px-1 ${wordClass} ${boldTextClass}`}
+          >
+            {` - `}
+          </motion.span>
+        </span>
+      )}
+      {splittext !== undefined && normalTextArray}
     </div>
   );
 }
